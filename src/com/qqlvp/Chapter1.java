f5527c2f249667482ee846863b650deb4bcf1171
MengZhiZhi <1814114667@qq.com>
2020-01-03 10:50:07 +0800

    1-3 程序示例

--- src/com/qqlvp/Chapter1.java
+++ src/com/qqlvp/Chapter1.java
@@ -1,7 +1,8 @@
 package com.qqlvp;
 // 第一章示例代码
 public class Chapter1 {
+    // 1-3 程序示例章节(1)
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");  // 打印输出字符串"HelloWorld!"
     }
 }
