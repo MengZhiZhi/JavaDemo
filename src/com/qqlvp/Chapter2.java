f620258ba85904aaf9b5c67fa3fbea0cfde29021
MengZhiZhi <1814114667@qq.com>
2020-01-03 11:35:08 +0800

    第二章

---
+++ src/com/qqlvp/Chapter2.java
@@ -0,0 +1,17 @@
+package com.qqlvp;
+// 第二章示例代码
+public class Chapter2 {
+    // 1-3 程序示例章节(1)
+    public static void main(String[] args) {
+        Chapter2.PrintIntegerMaxAndMinValue();
+    }
+
+    // 输出整型类型的最大值和最小值
+    public static void PrintIntegerMaxAndMinValue(){
+        // Int 类型的最大值和最小值
+        System.out.println(Integer.MAX_VALUE);  // 输出： 2147483647
+        System.out.println(Integer.MIN_VALUE);  // 输出： -2147483648
+        
+
+    }
+}
